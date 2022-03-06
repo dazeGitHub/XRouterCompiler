a7707914ae3cad8b1a2bba808995d5fdd3f5eaed
mayundaze <1992330573@qq.com>
2022-03-06 17:23:02 +0800

    first commit

---
+++ app/src/main/java/com/zyz/xroutercompiler/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.zyz.xroutercompiler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.toys.annotation.BindPath
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
